b929da3b023792fd1fdc59a34712a92728581105
Sergej Kasper <sergejkasper@gmail.com>
2015-06-30 01:32:12 +0200

    Update 2015-06-29T23:32:11.077Z

--- angular-get-bible.js
+++ angular-get-bible.js
@@ -24,55 +24,6 @@
 
 })(angular);
 
-angular.module('angularGetBible.services').provider('GetBibleService', function() {
-    function GetBibleService($http) {
-        this.getVerse = function getVerse(book, chapter, verse, version) {
-            book = book || '';
-            chapter = chapter || '';
-            verse = (verse) ? ':' + verse.toString() : '';
-            version = version || 'kjv';
-            return $http.jsonp('http://getbible.net/json?getbible=JSON_CALLBACK', {
-                params: {
-                    'p': book + chapter + verse,
-                    'v': version
-                }
-            });
-        };
-        this.getVerses = function getVerses(book, chapters, version) {
-            return this.getVerse(book, chapters, false, version);
-        };
-        this.getBooks = function getBooks(version) {
-            return $http.jsonp('http://getbible.net/json?getbible=', {
-                params: {
-                    'callback': 'JSON_CALLBACK',
-                    'option': 'com_getbible',
-                    'task': 'bible.books',
-                    'format': 'json',
-                    'v': version
-                }
-            });
-        };
-        this.getChapters = function getBooks(bookNr, version) {
-            return $http.jsonp('http://getbible.net/json?getbible=', {
-                params: {
-                    'callback': 'JSON_CALLBACK',
-                    'option': 'com_getbible',
-                    'task': 'bible.chapter',
-                    'format': 'json',
-                    'v': version,
-                    'nr': bookNr
-                }
-            });
-        };
-    }
-
-    this.$get = ['$http',
-        function($http) {
-            return new GetBibleService($http);
-        }
-    ];
-
-});
 angular.module('angularGetBible.directives').directive('getBibleNavigation', function($templateCache, GetBibleService) {
     return {
         scope: {
@@ -143,6 +94,52 @@ angular.module('angularGetBible.directives').directive('getBibleNavigation', fun
         template: $templateCache.get('angular-get-bible/templates/Navigation.template.html')
     };
 });
+angular.module('angularGetBible.directives').directive('getBibleVerseLinkView', function ($templateCache){
+  return {
+    restrict: 'AE',
+    transclude: true,
+    scope: true,
+    replace: false,
+    controller: function($scope) {
+      $scope.showing = false;
+      $scope.book = 'John';
+      $scope.chapter = '3';
+      $scope['verse_nr'] = [16];
+      this.showScripture = function(book, chapter, verses){
+        $scope.$apply(function(){
+          $scope.showing = true;
+          $scope.book = book;
+          $scope.chapter = chapter;
+          $scope['verse_nr'] = verses;
+        });
+      };
+      this.setShow = function(show){
+        $scope.$apply(function(){
+          $scope.showing = show;
+        });
+      };
+    },
+    template: $templateCache.get('angular-get-bible/templates/VerseLinkView.template.html')
+  };
+}).directive('getBibleVerseLink', function (){
+  return {
+    require: '^getBibleVerseLinkView',
+    restrict: 'AC',
+    link: function (scope, element, attrs, getBibleVerseContentCtrl) {
+      element.on('click', function(event) {
+          event.preventDefault();
+          var res = {}, 
+          d = decodeURIComponent;
+          event.target.search.substring(1).split('&').forEach(function(s){ 
+            var search = s.split('='); res[d(search[0])] = d(search[1]);
+          });
+          getBibleVerseContentCtrl.showScripture(res.b, res.chNr, JSON.parse(res.vNrs));
+          getBibleVerseContentCtrl.setShow(true);
+      });
+      
+    }
+  };
+});
 angular.module('angularGetBible.directives').directive('getBibleViewVerses', function($templateCache, GetBibleService) {
     return {
         scope: {
@@ -173,4 +170,53 @@ angular.module('angularGetBible.directives').directive('getBibleViewVerses', fun
         },
         template:  $templateCache.get('angular-get-bible/templates/ViewVerses.template.html')
     };
+});
+angular.module('angularGetBible.services').provider('GetBibleService', function() {
+    function GetBibleService($http) {
+        this.getVerse = function getVerse(book, chapter, verse, version) {
+            book = book || '';
+            chapter = chapter || '';
+            verse = (verse) ? ':' + verse.toString() : '';
+            version = version || 'kjv';
+            return $http.jsonp('http://getbible.net/json?getbible=JSON_CALLBACK', {
+                params: {
+                    'p': book + chapter + verse,
+                    'v': version
+                }
+            });
+        };
+        this.getVerses = function getVerses(book, chapters, version) {
+            return this.getVerse(book, chapters, false, version);
+        };
+        this.getBooks = function getBooks(version) {
+            return $http.jsonp('http://getbible.net/json?getbible=', {
+                params: {
+                    'callback': 'JSON_CALLBACK',
+                    'option': 'com_getbible',
+                    'task': 'bible.books',
+                    'format': 'json',
+                    'v': version
+                }
+            });
+        };
+        this.getChapters = function getBooks(bookNr, version) {
+            return $http.jsonp('http://getbible.net/json?getbible=', {
+                params: {
+                    'callback': 'JSON_CALLBACK',
+                    'option': 'com_getbible',
+                    'task': 'bible.chapter',
+                    'format': 'json',
+                    'v': version,
+                    'nr': bookNr
+                }
+            });
+        };
+    }
+
+    this.$get = ['$http',
+        function($http) {
+            return new GetBibleService($http);
+        }
+    ];
+
 });

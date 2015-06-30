c0c8140f6323e1a2e2a725f31e7e5aefd953dfcc
Sergej Kasper <sergejkasper@gmail.com>
2015-07-01 01:43:43 +0200

    add verse links

--- dist/angular-get-bible.js
+++ dist/angular-get-bible.js
@@ -24,6 +24,55 @@
 
 })(angular);
 
+angular.module('angularGetBible.services').provider('GetBibleService', function() {
+    function GetBibleService($http) {
+        this.getVerse = function getVerse(book, chapter, verse, version) {
+            book = book || '';
+            chapter = chapter || '';
+            verse = (verse) ? ':' + verse.toString() : '';
+            version = version || 'kjv';
+            return $http.jsonp('http://getbible.net/json?getbible=JSON_CALLBACK', {
+                params: {
+                    'p': book + chapter + verse,
+                    'v': version
+                }
+            });
+        };
+        this.getVerses = function getVerses(book, chapters, version) {
+            return this.getVerse(book, chapters, false, version);
+        };
+        this.getBooks = function getBooks(version) {
+            return $http.jsonp('http://getbible.net/json?getbible=', {
+                params: {
+                    'callback': 'JSON_CALLBACK',
+                    'option': 'com_getbible',
+                    'task': 'bible.books',
+                    'format': 'json',
+                    'v': version
+                }
+            });
+        };
+        this.getChapters = function getBooks(bookNr, version) {
+            return $http.jsonp('http://getbible.net/json?getbible=', {
+                params: {
+                    'callback': 'JSON_CALLBACK',
+                    'option': 'com_getbible',
+                    'task': 'bible.chapter',
+                    'format': 'json',
+                    'v': version,
+                    'nr': bookNr
+                }
+            });
+        };
+    }
+
+    this.$get = ['$http',
+        function($http) {
+            return new GetBibleService($http);
+        }
+    ];
+
+});
 angular.module('angularGetBible.directives').directive('getBibleNavigation', function($templateCache, GetBibleService) {
     return {
         scope: {
@@ -170,53 +219,4 @@ angular.module('angularGetBible.directives').directive('getBibleViewVerses', fun
         },
         template:  $templateCache.get('angular-get-bible/templates/ViewVerses.template.html')
     };
-});
-angular.module('angularGetBible.services').provider('GetBibleService', function() {
-    function GetBibleService($http) {
-        this.getVerse = function getVerse(book, chapter, verse, version) {
-            book = book || '';
-            chapter = chapter || '';
-            verse = (verse) ? ':' + verse.toString() : '';
-            version = version || 'kjv';
-            return $http.jsonp('http://getbible.net/json?getbible=JSON_CALLBACK', {
-                params: {
-                    'p': book + chapter + verse,
-                    'v': version
-                }
-            });
-        };
-        this.getVerses = function getVerses(book, chapters, version) {
-            return this.getVerse(book, chapters, false, version);
-        };
-        this.getBooks = function getBooks(version) {
-            return $http.jsonp('http://getbible.net/json?getbible=', {
-                params: {
-                    'callback': 'JSON_CALLBACK',
-                    'option': 'com_getbible',
-                    'task': 'bible.books',
-                    'format': 'json',
-                    'v': version
-                }
-            });
-        };
-        this.getChapters = function getBooks(bookNr, version) {
-            return $http.jsonp('http://getbible.net/json?getbible=', {
-                params: {
-                    'callback': 'JSON_CALLBACK',
-                    'option': 'com_getbible',
-                    'task': 'bible.chapter',
-                    'format': 'json',
-                    'v': version,
-                    'nr': bookNr
-                }
-            });
-        };
-    }
-
-    this.$get = ['$http',
-        function($http) {
-            return new GetBibleService($http);
-        }
-    ];
-
 });
